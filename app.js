8e2c9c5766356f515b58260717c63e2f9c621aae
Alex Choi <heyalexchoi@gmail.com>
2015-04-28 21:51:26 -0400

    compression

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compress = require('compression');
 var helper = require('./hnhelper');
 var routes = require('./routes/index');
 var app = express();
@@ -13,11 +14,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
 app.use(favicon(__dirname + '/public/images/dog.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compress())
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
